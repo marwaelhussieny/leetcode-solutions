4fb6b3c45c50e4dd02ea9dabf03bd0e302daef16
Marwa El-hussieny <99705813+marwaelhussieny@users.noreply.github.com>
2026-08-01 03:11:32 +0300

    Time: 541 ms (29.24%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,5 @@
 SELECT W1.ID
 FROM WEATHER W2
 INNER JOIN WEATHER W1
-    ON W2.recordDate = DATEADD(DAY, -1, W1.recordDate)
+    ON DATEDIFF(DAY, W2.recordDate, W1.recordDate) = 1
 WHERE W1.temperature > W2.temperature;
